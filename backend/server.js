a152d4c3a1bb5a8ad3e1391622d5733fcdfabbae
aathshamehar <aathsha@gmail.com>
2023-06-18 15:44:36 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,100 @@
+
 const express = require('express');
 const app = express();
-const PORT = 4587;
-const mongoose = require('mongoose');
+const PORT = 4578 //PORT
+const mongoose = require('mongoose'); //mongoose for DB
+const cors = require('cors'); // to remore cor issue
+app.use(cors())  // cor policy activation
+app.use(express.json()); // to render json req from frontend
+app.use(express.urlencoded({ extended: true })); // to render form data from frontend
+
+//connect backend with DB
+mongoose.connect("mongodb+srv://31860noor:hP8))GNr-S6-Cja@cluster0.twzrvkq.mongodb.net/")//!insert your username
+    .then(() => { console.log("MongoDB connected successfully") })//*success message
+    .catch((err) => { console.log("Error connecting to MongoDB " + err) });//*fail message
+
+// CRUD operation 
+// C-Create - POST 
+//R-READ-GET
+//U-Update -PUT
+//D-Delete - DELETE
 
 
-mongoose.connect("mongodb+srv://31860noor:hP8))GNr-S6-Cja@cluster0.twzrvkq.mongodb.net/").
-then(() => {console.log("MongoDB connected successfully")}).catch(() => {console.log("Error connecting to MongoDB")});
+const PRODUCT = require("./model/product")
 
-app.get('/', (req,res) => {
-    res.send("<h1>Server Running</h1>");
+
+//CREATE
+app.post('/addData', async (req, res) => {
+    try {
+
+        let item = req.body
+        console.log(item)
+
+
+        const saveData = await PRODUCT(item) //PRODUCT is a model. we cross check the form data with the model we created before saving it
+        await saveData.save()  // through this code we save the incoming data from front end to db 
+        res.send((saveData))
+
+
+    } catch (error) {
+        res.send(error);
+    }
 })
 
-app.listen(3000, () =>{
-    console.log(`Server running on ${PORT}`);
-}); 
+//READ
+
+app.get('/getData', async (req, res) => {
+    try {
+        console.log("reading data")
+        const data = await PRODUCT.find({}) //code that find all data from backend
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
+})
+
+
+// For deleteing and updating we are using "ids" of entry and then do the procedd
+
+//UPDATE
+
+
+app.put('/updateData/:id', async (req, res) => {
+    try {
+
+        let id = req.params.id
+        let updateData = { $set: req.body }
+    
+        const updated = await PRODUCT.findByIdAndUpdate(id, updateData)
+    
+        res.json(updated)
+        
+    }  catch (error) {
+        console.log(error)
+        res.send('error')
+    }
+
+})
+
+
+//DELETE
+app.delete('/deteData/:id', async (req, res) => {
+    try {
+        let id = req.params.id
+        console.log('id check', id)
+
+        const updated = await PRODUCT.findByIdAndDelete(id)
+
+        res.send("deleted successfully")
+
+    } catch (error) {
+        console.log(error)
+        res.send('error')
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
